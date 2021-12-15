19010341e0e7ca70f5e383e7e99ea29a29ef8f36
Max <linme00p@gmail.com>
2021-12-15 13:41:40 +0200

    Create echo bot.

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '1738335937:AAGHDAgXYGeBnW50kMP0SH7hMeIvkNBLQG4'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
 if __name__ == '__main__':
-    print('Hello World!')
+    executor.start_polling(dp, skip_updates=True)
